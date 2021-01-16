b85323d9cbd57a7ca84bb9335f0387ee201a18dd
shuxinqin <shuxinqin@outlook.com>
2021-01-16 17:34:12 +0800

    .

--- src/Chloe/RDBMS/IMethodHandler.cs
+++ src/Chloe/RDBMS/IMethodHandler.cs
@@ -4,7 +4,18 @@ namespace Chloe.RDBMS
 {
     public interface IMethodHandler
     {
+        /// <summary>
+        /// 判断是否可以解析传入的方法。
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns></returns>
         bool CanProcess(DbMethodCallExpression exp);
+
+        /// <summary>
+        /// 解析传入的方法。
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <param name="generator"></param>
         void Process(DbMethodCallExpression exp, SqlGeneratorBase generator);
     }
 }
