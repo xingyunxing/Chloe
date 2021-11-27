39b4411d5acccfc94534531bf38798f1c62a919b
shuxinqin <shuxinqin@outlook.com>
2021-11-27 14:55:18 +0800

    v4.18.0

--- src/tools/Chloe-All.bat
+++ src/tools/Chloe-All.bat
@@ -1,8 +1,10 @@
+
 .\nuget pack Chloe.nuspec
 .\nuget pack Chloe.Extension.nuspec
 .\nuget pack Chloe.MySql.nuspec
 .\nuget pack Chloe.Oracle.nuspec
+.\nuget pack Chloe.PostgreSQL.nuspec
 .\nuget pack Chloe.SQLite.nuspec
 .\nuget pack Chloe.SqlServer.nuspec
-.\nuget pack Chloe.PostgreSQL.nuspec
+
 pause
