939610aa8126de1e5d854d46eb1f78d2a8d4849f
shuxinqin <shuxinqin@outlook.com>
2021-12-28 23:31:45 +0800

    update Chloe.Extension

--- src/Chloe.Extension/Utils.cs
+++ src/Chloe.Extension/Utils.cs
@@ -4,7 +4,18 @@
     {
         public static DbParam[] BuildParams(IDbContext dbContext, object parameter)
         {
-            return PublicHelper.BuildParams((DbContext)dbContext, parameter);
+            DbContext context = dbContext as DbContext;
+            if (context == null)
+            {
+                var holdDbContextProp = dbContext.GetType().GetProperty("HoldDbContext");
+                if (holdDbContextProp != null)
+                {
+                    dbContext = Chloe.Reflection.ReflectionExtension.FastGetMemberValue(holdDbContextProp, dbContext) as IDbContext;
+                    return BuildParams(dbContext, parameter);
+                }
+            }
+
+            return PublicHelper.BuildParams(context, parameter);
         }
     }
 }
