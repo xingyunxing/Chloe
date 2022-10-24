735b253c42790b15ee1f36ad223b0a5c6068bfdf
shuxinqin <shuxinqin@outlook.com>
2022-10-25 01:06:46 +0800

    code fixed.

--- src/Chloe.Oracle/OracleSemantics.cs
+++ src/Chloe.Oracle/OracleSemantics.cs
@@ -5,7 +5,7 @@ namespace Chloe.Oracle
 {
     public static class OracleSemantics
     {
-        internal static readonly PropertyInfo PropertyInfo_ROWNUM = typeof(OracleSemantics).GetProperty("ROWNUM");
+        internal static readonly PropertyInfo PropertyInfo_ROWNUM = typeof(OracleSemantics).GetProperty(nameof(OracleSemantics.ROWNUM));
         internal static readonly DbMemberExpression DbMemberExpression_ROWNUM = DbExpression.MemberAccess(OracleSemantics.PropertyInfo_ROWNUM, null);
 
         public static decimal ROWNUM
