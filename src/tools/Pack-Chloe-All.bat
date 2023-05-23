43dc494842145ee17131ee7d2baff15222c753d4
shuxinqin <shuxinqin@outlook.com>
2023-05-24 00:35:19 +0800

    .

--- src/tools/Pack-Chloe-All.bat
+++ src/tools/Pack-Chloe-All.bat
@@ -1,9 +1,29 @@
 
-cd ..
-dotnet clean -c Release
-dotnet build -c Release
 
-cd tools
+dotnet clean -c Release ../Chloe/Chloe.csproj
+dotnet build -c Release ../Chloe/Chloe.csproj
+
+dotnet clean -c Release ../Chloe.Extension/Chloe.Extension.csproj
+dotnet build -c Release ../Chloe.Extension/Chloe.Extension.csproj
+
+dotnet clean -c Release ../Chloe.Dameng/Chloe.Dameng.csproj
+dotnet build -c Release ../Chloe.Dameng/Chloe.Dameng.csproj
+
+dotnet clean -c Release ../Chloe.MySql/Chloe.MySql.csproj
+dotnet build -c Release ../Chloe.MySql/Chloe.MySql.csproj
+
+dotnet clean -c Release ../Chloe.Oracle/Chloe.Oracle.csproj
+dotnet build -c Release ../Chloe.Oracle/Chloe.Oracle.csproj
+
+dotnet clean -c Release ../Chloe.PostgreSQL/Chloe.PostgreSQL.csproj
+dotnet build -c Release ../Chloe.PostgreSQL/Chloe.PostgreSQL.csproj
+
+dotnet clean -c Release ../Chloe.SQLite/Chloe.SQLite.csproj
+dotnet build -c Release ../Chloe.SQLite/Chloe.SQLite.csproj
+
+dotnet clean -c Release ../Chloe.SqlServer/Chloe.SqlServer.csproj
+dotnet build -c Release ../Chloe.SqlServer/Chloe.SqlServer.csproj
+
 
 del /f /s /q packages\*
 rd packages
