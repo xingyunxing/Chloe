3a7bd57a8cc55f9b897780705f4c9125f882cc9c
hongyl <hongyl@xmeport.cn>
2023-06-19 11:41:11 +0800

    加入排除字段对匿名类型的支持

---
+++ src/Chloe/Mapper/Activators/NullValueObjectActivator.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Chloe.Mapper.Activators
+{
+    public class NullValueObjectActivator : IObjectActivator
+    {
+        private object _value;
+
+        public NullValueObjectActivator(Type primitiveType)
+        {
+            _value = primitiveType.IsValueType ? Activator.CreateInstance(primitiveType) : null;
+        }
+
+        public virtual void Prepare(IDataReader reader)
+        {
+        }
+
+        public async ObjectResultTask CreateInstance(IDataReader reader, bool @async) => _value;
+    }
+}
