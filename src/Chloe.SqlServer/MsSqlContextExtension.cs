3c7227fd5950dcad3e6818fee617539f33aa1869
shuxinqin <shuxinqin@outlook.com>
2023-12-03 00:01:19 +0800

    add MsSqlContextExtension.BulkInsertAsync()

--- src/Chloe.SqlServer/MsSqlContextExtension.cs
+++ src/Chloe.SqlServer/MsSqlContextExtension.cs
@@ -1,12 +1,19 @@
-﻿namespace Chloe.SqlServer
+﻿using System.Threading.Tasks;
+
+namespace Chloe.SqlServer
 {
     public static class MsSqlContextExtension
     {
         public static void BulkInsert<TEntity>(this IDbContext dbContext, List<TEntity> entities, string table = null, int? batchSize = null, int? bulkCopyTimeout = null, bool keepIdentity = false)
         {
             MsSqlContext msSqlContext = (MsSqlContext)dbContext;
-            MsSqlContextProvider msSqlContextProvider = (MsSqlContextProvider)msSqlContext.DefaultDbContextProvider;
-            msSqlContextProvider.BulkInsert<TEntity>(entities, table, batchSize, bulkCopyTimeout, keepIdentity);
+            msSqlContext.BulkInsert<TEntity>(entities, table, batchSize, bulkCopyTimeout, keepIdentity);
+        }
+
+        public static async Task BulkInsertAsync<TEntity>(this IDbContext dbContext, List<TEntity> entities, string table = null, int? batchSize = null, int? bulkCopyTimeout = null, bool keepIdentity = false)
+        {
+            MsSqlContext msSqlContext = (MsSqlContext)dbContext;
+            await msSqlContext.BulkInsertAsync<TEntity>(entities, table, batchSize, bulkCopyTimeout, keepIdentity);
         }
     }
 }
