572d9c345fd616dc52ed2f69b3fa1f4933dbf1a2
qinshuxin <so_while@163.com>
2024-08-21 14:39:35 +0800

    修改mysql中 in 里面不支持 limit 问题

--- src/Chloe.MySql/MethodHandlers/Contains_Handler.cs
+++ src/Chloe.MySql/MethodHandlers/Contains_Handler.cs
@@ -17,5 +17,35 @@ namespace Chloe.MySql.MethodHandlers
             generator.SqlBuilder.Append(",'%'");
             generator.SqlBuilder.Append(")");
         }
+
+        /// <summary>
+        /// in 子查询
+        /// </summary>
+        /// <param name="generator"></param>
+        /// <param name="sqlQuery"></param>
+        /// <param name="operand"></param>
+        protected override void In(SqlGeneratorBase generator, DbSqlQueryExpression sqlQuery, DbExpression operand)
+        {
+            if (sqlQuery.SkipCount != null || sqlQuery.TakeCount != null)
+            {
+                /*
+                 * mysql 中不支持 `T`.`CityId` IN (SELECT `T0`.`Id` AS `C` FROM `City` AS `T0` LIMIT 0,10)，操蛋！
+                 * 需要改成 `T`.`CityId` IN (SELECT * FROM (SELECT `T0`.`Id` AS `C` FROM `City` AS `T0` LIMIT 0,10) AS T_T)
+                 */
+                operand.Accept(generator);
+                generator.SqlBuilder.Append(" IN (");
+                generator.SqlBuilder.Append("SELECT * FROM (");
+                sqlQuery.Accept(generator);
+                generator.SqlBuilder.Append(") AS T_T");
+                generator.SqlBuilder.Append(")");
+
+                return;
+            }
+
+            operand.Accept(generator);
+            generator.SqlBuilder.Append(" IN (");
+            sqlQuery.Accept(generator);
+            generator.SqlBuilder.Append(")");
+        }
     }
 }
