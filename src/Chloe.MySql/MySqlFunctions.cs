0865d503d83381b6442f450acca0a8bda16a84d8
qinshuxin <so_while@163.com>
2024-09-22 23:04:33 +0800

    MySqlFunctions.FindInSet()

---
+++ src/Chloe.MySql/MySqlFunctions.cs
@@ -0,0 +1,13 @@
+﻿using Chloe.Annotations;
+
+namespace Chloe.MySql
+{
+    public static class MySqlFunctions
+    {
+        [DbFunctionAttribute("FIND_IN_SET")]
+        public static bool FindInSet(string str, string strList)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
