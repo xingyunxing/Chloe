74aab1031a64afd56900dd2388c85fef820b4e16
qinshuxin <so_while@163.com>
2024-10-01 03:16:20 +0800

    add comment.

--- src/Chloe/Sharding/ShardingOptions.cs
+++ src/Chloe/Sharding/ShardingOptions.cs
@@ -7,7 +7,8 @@ namespace Chloe.Sharding
     public class ShardingOptions
     {
         /// <summary>
-        /// 每次查询单个数据库最多可打开的连接数
+        /// 每次查询单个数据库最多可打开的连接数。
+        /// 注：如果类似 SQLite 不支持多线程读写的数据库，一定要将此属性值设置为 1。
         /// </summary>
         public int MaxConnectionsPerDataSource { get; set; } = 12;
     }
